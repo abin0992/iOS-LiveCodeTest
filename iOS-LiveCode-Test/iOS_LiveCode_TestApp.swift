caf0a7651ed32f0484fe3b7deecd3a5d9f5f37c7
Christine Ramos <tineramos.ph@gmail.com>
2023-07-20 12:49:37 +0100

    Initial commit

---
+++ iOS-LiveCode-Test/iOS_LiveCode_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_LiveCode_TestApp.swift
+//  iOS-LiveCode-Test
+//
+//  Created by Christine Ramos on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_LiveCode_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
